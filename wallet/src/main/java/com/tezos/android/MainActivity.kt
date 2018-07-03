635dd4d0a63a2f6f6ab1c4551c55943970f27142
Nicolas FILLION <nfillion21@gmail.com>
2018-07-03 17:13:33 +0200

    Add drawer layout

--- wallet/src/main/java/com/tezos/android/MainActivity.kt
+++ wallet/src/main/java/com/tezos/android/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.tezos.android
 
-import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
+import android.support.design.widget.NavigationView
 import android.support.v4.content.ContextCompat
-import android.support.v4.graphics.drawable.DrawableCompat
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
 import com.tezos.core.models.CustomTheme
@@ -17,9 +20,9 @@ import com.tezos.ui.activity.CreateWalletActivity
 import com.tezos.ui.activity.PaymentScreenActivity
 import com.tezos.ui.activity.RestoreWalletActivity
 import com.tezos.ui.utils.Utils
-import com.tezos.ui.utils.Utils.makeSelector
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity()
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener
 {
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -98,6 +101,13 @@ class MainActivity : AppCompatActivity()
 
         val titleBar = findViewById<TextView>(R.id.barTitle)
         titleBar.setTextColor(ContextCompat.getColor(this, theme.textColorPrimaryId))
+
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
     }
 
     private fun validatePayButton(button:Button, theme:CustomTheme) {
@@ -109,5 +119,57 @@ class MainActivity : AppCompatActivity()
         val drawables = button.compoundDrawables
         //val wrapDrawable = DrawableCompat.wrap(drawables[0])
         //DrawableCompat.setTint(wrapDrawable, ContextCompat.getColor(this, theme.textColorPrimaryId))
+    }
+
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_camera -> {
+                // Handle the camera action
+            }
+            R.id.nav_gallery -> {
+
+            }
+            R.id.nav_slideshow -> {
+
+            }
+            R.id.nav_manage -> {
+
+            }
+            R.id.nav_share -> {
+
+            }
+            R.id.nav_send -> {
+
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
