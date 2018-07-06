3d54d0e83e1e5eb3d5ae080a824f6c6964c969a6
Nicolas FILLION <nfillion21@gmail.com>
2018-07-06 20:15:46 +0200

    Reorder drawer menu

--- wallet/src/main/java/com/tezos/android/MainActivity.kt
+++ wallet/src/main/java/com/tezos/android/MainActivity.kt
@@ -10,7 +10,6 @@ import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
@@ -206,7 +205,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             {
                 AddAddressActivity.start(this, tezosTheme)
             }
-            R.id.nav_send ->
+            R.id.nav_info ->
             {
 
             }
