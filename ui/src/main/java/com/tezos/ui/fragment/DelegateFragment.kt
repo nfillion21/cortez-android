264764ba649932031090697facde70fa15dd6bc6
Nicolas FILLION <nfillion21@gmail.com>
2018-11-19 18:30:27 +0100

    Improve delegation form

--- ui/src/main/java/com/tezos/ui/fragment/DelegateFragment.kt
+++ ui/src/main/java/com/tezos/ui/fragment/DelegateFragment.kt
@@ -27,11 +27,18 @@
 
 package com.tezos.ui.fragment
 
+import android.content.Intent.getIntent
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.StateListDrawable
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
+import android.support.v4.graphics.drawable.DrawableCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.tezos.core.models.CustomTheme
@@ -44,6 +51,9 @@ class DelegateFragment : Fragment()
     private var mPkhTextview: TextView? = null
 
     private var mPkhEmptyLayout: LinearLayout? = null
+
+    private var mAddDelegateButton: Button? = null
+    private var mAddDelegateButtonLayout: FrameLayout? = null
 
     companion object
     {
@@ -60,16 +70,22 @@ class DelegateFragment : Fragment()
     {
         super.onViewCreated(view, savedInstanceState)
 
-        arguments?.let {
-            val themeBundle = it.getBundle(CustomTheme.TAG)
-        }
-
         mPkhEmptyLayout = view.findViewById(R.id.pkh_empty_layout)
 
         mLinearLayout = view.findViewById(R.id.pkh_info_layout)
 
         mPkhTextview = view.findViewById(R.id.pkh_textview)
 
+        mAddDelegateButton = view.findViewById(R.id.add_delegate_button)
+        mAddDelegateButtonLayout = view.findViewById(R.id.add_delegate_button_layout)
+
+        mAddDelegateButtonLayout?.setOnClickListener { v ->
+        }
+
+        arguments?.let {
+            val themeBundle = it.getBundle(CustomTheme.TAG)
+            validateAddButton(isInputDataValid(), CustomTheme.fromBundle(themeBundle))
+        }
     }
 
     override fun onResume()
@@ -90,5 +106,44 @@ class DelegateFragment : Fragment()
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
     {
         return inflater.inflate(R.layout.fragment_delegate, container, false)
+    }
+
+    private fun validateAddButton(validate: Boolean, theme: CustomTheme)
+    {
+        if (validate)
+        {
+            mAddDelegateButton?.setTextColor(ContextCompat.getColor(activity!!, theme.textColorPrimaryId))
+            mAddDelegateButtonLayout?.isEnabled = true
+            mAddDelegateButtonLayout?.background = makeSelector(theme)
+
+            val drawables = mAddDelegateButton?.compoundDrawables
+            val wrapDrawable = DrawableCompat.wrap(drawables!![0])
+            DrawableCompat.setTint(wrapDrawable, ContextCompat.getColor(activity!!, theme.textColorPrimaryId))
+        }
+        else
+        {
+            mAddDelegateButton?.setTextColor(ContextCompat.getColor(activity!!, android.R.color.white))
+            mAddDelegateButtonLayout?.isEnabled = false
+
+            val greyTheme = CustomTheme(R.color.dark_grey, R.color.dark_grey, R.color.dark_grey)
+            mAddDelegateButtonLayout?.background = makeSelector(greyTheme)
+
+            val drawables = mAddDelegateButton?.compoundDrawables
+            val wrapDrawable = DrawableCompat.wrap(drawables!![0])
+            DrawableCompat.setTint(wrapDrawable, ContextCompat.getColor(activity!!, android.R.color.white))
+        }
+    }
+
+    private fun isInputDataValid(): Boolean
+    {
+        return true
+    }
+
+    private fun makeSelector(theme: CustomTheme): StateListDrawable
+    {
+        val res = StateListDrawable()
+        res.addState(intArrayOf(android.R.attr.state_pressed), ColorDrawable(ContextCompat.getColor(activity!!, theme.colorPrimaryDarkId)))
+        res.addState(intArrayOf(), ColorDrawable(ContextCompat.getColor(activity!!, theme.colorPrimaryId)))
+        return res
     }
 }
